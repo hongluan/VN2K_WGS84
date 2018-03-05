345a8d804544796a160f9d4d76d762890f22e1e2
hongluan <33394481+hongluan@users.noreply.github.com>
2018-03-05 13:08:43 +0700

    FINISH FUNCTION VN2K - WGS84

--- VECT_TRANS.py
+++ VECT_TRANS.py
@@ -5,36 +5,79 @@ Created on Sun Mar  4 12:14:22 2018
 @author: PHẠM ĐẶNG MẠNH HỒNG LUÂN
 """
 
-from osgeo import gdal,ogr
+from osgeo import gdal,ogr,osr
+import os,sys
 
-def readvect():
-    inshpdir = input('INPUT VECTOR FILE: ')
-    driver = ogr.GetDriverByName('ESRI Shapefile')
-    dataset = driver.Open(inshpdir)
-    layer = dataset.GetLayer()
-    return layer
-def readgeoref():
+inshpdir = input('INPUT VECTOR FILE: ')
+driver = ogr.GetDriverByName('ESRI Shapefile')
+dataset = driver.Open(inshpdir)
+layer = dataset.GetLayer()
+def exportgeomwtk(layer):
+    featlst = []
+    for feat in layer:
+        wkt = feat.GetGeometryRef().ExportToWkt()
+        featlst.append(wkt)
+    return featlst
+def spatrefdef(layer):    
+    spatref = layer.GetSpatialRef()
+    print('Spatial reference of your input vector is: {}'.format(spatref))
+    return spatref
+def importfield(layer,outLayer):
+    inLayerDefn = layer.GetLayerDefn()
+    for i in range(0,inLayerDefn.GetFieldCount()):
+        fieldDefn = inLayerDefn.GetFieldDefn(i)
+        outLayer.CreateField(fieldDefn)
+        print(i)
+    outLayerDefn = outLayer.GetLayerDefn()
+    return outLayerDefn
+def newvect(layer):    
+    newfulname = inshpdir.split('.')[-2] + '_Trans.shp'
+    newname = inshpdir.split('.')[-2].split('\\')[-1]
     
-    return
-
-
-
-
+    outdataset = driver.CreateDataSource(newfulname)
+    outlayer = outdataset.CreateLayer(newname+'_Trans',geom_type = ogr.wkbPolygon)    
+    return outlayer
+def VN2K2WGS84(layer):
+        
+    prj = open('D:\\Tai_Lieu\\Tai_Lieu_Hoc\\GIS\\VN2K\\3405 (2).prj').read()
+    vnspatref = osr.SpatialReference()
+    vnspatref.ImportFromWkt(prj)
+    wgsspatref = osr.SpatialReference()
+    wgsspatref.ImportFromEPSG(4326)
+    transform = osr.CoordinateTransformation(vnspatref,wgsspatref)
+    
+    newfulname = inshpdir.split('.')[-2] + '_Trans.shp'
+    
+    newname = inshpdir.split('.')[-2].split('\\')[-1]
+    
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    outdataset = driver.CreateDataSource(newfulname)
+    outLayer = outdataset.CreateLayer(newname+'_Trans',geom_type = ogr.wkbPolygon)    
+    inLayerDefn = layer.GetLayerDefn()    
+    for i in range(0, inLayerDefn.GetFieldCount()):
+        fieldDefn = inLayerDefn.GetFieldDefn(i)
+        outLayer.CreateField(fieldDefn)
+    outLayerDefn = outLayer.GetLayerDefn()    
+    for feature in layer:    
+        geom = feature.GetGeometryRef()        
+        geom.Transform(transform)    
+        outFeature = ogr.Feature(outLayerDefn)    
+        outFeature.SetGeometry(geom)        
+        for i in range(0,outLayerDefn.GetFieldCount()):            
+            outFeature.SetField(outLayerDefn.GetFieldDefn(i).GetNameRef(), feature.GetField(i))    
+        outLayer.CreateFeature(outFeature)    
+        outFeature = None          
+    wgsspatref.MorphToESRI()
+    file = open(newname+'_Trans.prj', 'w') 
+    file.write(wgsspatref.ExportToWkt())
+    file.close()
 #MAIN MENU FUNCTION
 def main_menu():
     os.system('clear')
-    print('WELCOME TO lANDUSE CLASSIFICATION PROGRAM! \n')
+    print('WELCOME TO COORDINATE TRANSFORMATION PROGRAM! \n')
     print('PLEASE CHOOSE OPERATION YOU WANT: \n')
-    print('1. VISUALIZING INPUT DATA')
-    print('2. DRAWING HISTOGRAM OF TRAINING DATA')
-    print('3. COMPOSITE IMAGES')
-    print('4. UNSUPERVISED CLASSIFICATION')
-    print('5. SUPERVISED CLASSIFICATION')
-    print('6. POST CLASSIFICATION')
-    print('7. PCA')
-    print('8. EXPORT DATA')
-    print('9. COME BACK TO MAIN MENU')
-    print('0. TERMINATE THE PROGRAM')    
+    print('1. TRANSFORM VN2000 TO GEOGRAPHIC WGS84')
+        
     choice = input('>> ')
     exec_menu(choice)    
 ###################
@@ -54,33 +97,10 @@ def exec_menu(choice):
             if ch in ['9','0']:
                 menu_actions[ch]()
             elif ch == '1':
-                menu_actions[ch](imgarr,imgarrcldrmove,optname,subset,dtmbol,dsmbol,vegibol,trainarr,pcaimgarr,pcaimgarrcldrm)
+                menu_actions[ch](layer)                
                 choice = input('>> ')
                 exec_menu(choice)
-            elif ch == '2':
-                menu_actions[ch](imgarr,trainarr,optname,dsmbol,dtmbol,vegibol,pcaarr)
-                choice = input('>> ')
-                exec_menu(choice)
-            elif ch == '3':
-                menu_actions[ch](imgarr,optname)
-                choice = input('>> ')
-                exec_menu(choice)
-            elif ch == '4':
-                menu_actions[ch](imgarr)
-                choice = input('>> ')
-                exec_menu(choice)
-            elif ch == '5':
-                menu_actions[ch](imgarrcldrmove,trainarrcldfree,trainarr,subdir,pcabol,pcaimgarrcldrm)
-                choice = input('>> ')
-                exec_menu(choice)
-            elif ch == '7':
-                menu_actions[ch](imgarr)
-                choice = input('>> ')
-                exec_menu(choice)
-            elif ch == '8':
-                menu_actions[ch](imgarr,imgarrcldrmove,indir,subdir)
-                choice = input('>> ')
-                exec_menu(choice)               
+                           
         except KeyError:
             print('INVALID SECTION, PLEASE TRY AGAIN. \n')
             menu_actions['main_menu']()
@@ -98,13 +118,8 @@ def exit():
 #MENU DEFINITION
 menu_actions = {
         'main_menu': main_menu,
-        '1': visualinput,
-        '2': plottrainpair,
-        '3': plotcomposite,
-        '4': unsupervise,
-        '5': luclassify,
-        '7': pcatrans,
-        '8': export2imray,
+        '1': VN2K2WGS84,
+        
         '9': back,
         '0': exit}
 ###############
