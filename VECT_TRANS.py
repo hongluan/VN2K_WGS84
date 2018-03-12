0772c5d32a35b7dbc5181c28be3eab470cd05b41
hongluan <33394481+hongluan@users.noreply.github.com>
2018-03-12 07:55:00 +0700

    Edit

--- VECT_TRANS.py
+++ VECT_TRANS.py
@@ -30,11 +30,11 @@ def newvect(layer):
     outdataset = driver.CreateDataSource(newfulname)
     outlayer = outdataset.CreateLayer(newname+'_Trans',geom_type = ogr.wkbPolygon)    
     return outlayer
-def VN2K2WGS84(layer):        
+def PRJVN2K2WGS84(layer):        
     #prj = open('D:\\Tai_Lieu\\Tai_Lieu_Hoc\\GIS\\VN2K\\3405 (2).prj').read()
     centralmerid = input('PLEASE INPUT CENTRAL MERIDIAN OF INPUT FILE: ')
     scale = input('PLEASE CHOOSE SCALE OF INPUT FILE (0.9996/0.9999): ') 
-    prj = '''PROJCS["VN-2000 / UTM zone 48N", 
+    prj = '''PROJCS["VN-2000 /", 
     GEOGCS["VN-2000",_
         DATUM["Vietnam_2000",
             SPHEROID["WGS 84",6378137,298.257223563,
@@ -64,13 +64,13 @@ def VN2K2WGS84(layer):
     wgsspatref.ImportFromEPSG(4326)
     transform = osr.CoordinateTransformation(vnspatref,wgsspatref)
     
-    newfulname = inshpdir.split('.')[-2] + '_Trans.shp'
+    newfulname = inshpdir.split('.')[-2] + '_GEOWGS.shp'
     
     newname = inshpdir.split('.')[-2].split('\\')[-1]
     
     driver = ogr.GetDriverByName('ESRI Shapefile')
     outdataset = driver.CreateDataSource(newfulname)
-    outLayer = outdataset.CreateLayer(newname+'_Trans',geom_type = ogr.wkbPolygon)    
+    outLayer = outdataset.CreateLayer(newname+'_GEOWGS',geom_type = ogr.wkbPolygon)    
     inLayerDefn = layer.GetLayerDefn()    
     for i in range(0, inLayerDefn.GetFieldCount()):
         fieldDefn = inLayerDefn.GetFieldDefn(i)
@@ -86,15 +86,86 @@ def VN2K2WGS84(layer):
         outLayer.CreateFeature(outFeature)    
         outFeature = None          
     wgsspatref.MorphToESRI()
-    file = open(newname+'_Trans.prj', 'w') 
+    newfulnamenoext = newfulname.split('.')[-2]
+    file = open(newfulnamenoext+'.prj', 'w') 
     file.write(wgsspatref.ExportToWkt())
     file.close()
+###################
+
+#FUNCTION TRANSFORMATION FROM PROJECTED VN2000 TO UTM
+def PRJVN20002UTM(layer):
+    centralmerid = input('PLEASE INPUT CENTRAL MERIDIAN OF INPUT FILE: ')
+    scale = input('PLEASE CHOOSE SCALE OF INPUT FILE (0.9996/0.9999): ')
+    UTMcent = input('PLEASE CHOOSE UTM ZONE (48/49): ')
+    prj = '''PROJCS["VN-2000", 
+    GEOGCS["VN-2000",_
+        DATUM["Vietnam_2000",
+            SPHEROID["WGS 84",6378137,298.257223563,
+                AUTHORITY["EPSG","7030"]],
+            TOWGS84[-191.90441429,-39.30318279,-111.45032835,-0.00928836,0.01975479,-0.00427372,1.000000252906278],
+            AUTHORITY["EPSG","6756"]],
+        PRIMEM["Greenwich",0,
+            AUTHORITY["EPSG","8901"]],
+        UNIT["degree",0.0174532925199433,
+            AUTHORITY["EPSG","9122"]],
+        AUTHORITY["EPSG","4756"]],
+    PROJECTION["Transverse_Mercator"],
+    PARAMETER["latitude_of_origin",0],
+    PARAMETER["central_meridian",{}],
+    PARAMETER["scale_factor",{}],
+    PARAMETER["false_easting",500000],
+    PARAMETER["false_northing",0],
+    UNIT["metre",1,
+        AUTHORITY["EPSG","9001"]],
+    AXIS["Easting",EAST],
+    AXIS["Northing",NORTH],
+    AUTHORITY["EPSG","3405"]]'''.format(centralmerid,scale)
+    
+    vnspatref = osr.SpatialReference()
+    vnspatref.ImportFromWkt(prj)
+    wgsspatref = osr.SpatialReference()
+    if int(UTMcent) == 48:
+        wgsspatref.ImportFromEPSG(32648)
+    else:
+        wgsspatref.ImportFromEPSG(32649)
+    transform = osr.CoordinateTransformation(vnspatref,wgsspatref)
+    
+    newfulname = inshpdir.split('.')[-2] + '_UTM.shp'
+    
+    newname = inshpdir.split('.')[-2].split('\\')[-1]
+    
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    outdataset = driver.CreateDataSource(newfulname)
+    outLayer = outdataset.CreateLayer(newname+'_UTM',geom_type = ogr.wkbPolygon)    
+    inLayerDefn = layer.GetLayerDefn()    
+    for i in range(0, inLayerDefn.GetFieldCount()):
+        fieldDefn = inLayerDefn.GetFieldDefn(i)
+        outLayer.CreateField(fieldDefn)
+    outLayerDefn = outLayer.GetLayerDefn()    
+    for feature in layer:    
+        geom = feature.GetGeometryRef()        
+        geom.Transform(transform)    
+        outFeature = ogr.Feature(outLayerDefn)    
+        outFeature.SetGeometry(geom)        
+        for i in range(0,outLayerDefn.GetFieldCount()):            
+            outFeature.SetField(outLayerDefn.GetFieldDefn(i).GetNameRef(), feature.GetField(i))    
+        outLayer.CreateFeature(outFeature)    
+        outFeature = None          
+    wgsspatref.MorphToESRI()
+    newfulnamenoext = newfulname.split('.')[-2]
+    file = open(newfulnamenoext+'.prj', 'w') 
+    file.write(wgsspatref.ExportToWkt())
+    file.close()
+    
+###########################################
+    
 #MAIN MENU FUNCTION
 def main_menu():
     os.system('clear')
     print('WELCOME TO COORDINATE TRANSFORMATION PROGRAM! \n')
     print('PLEASE CHOOSE OPERATION YOU WANT: \n')
-    print('1. TRANSFORM VN2000 TO GEOGRAPHIC WGS84')        
+    print('1. TRANSFORM VN2000 TO GEOGRAPHIC WGS84')
+    print('2. TRANSFORM VN2000 TO UTM WGS84')        
     choice = input('>> ')
     exec_menu(choice)    
 ###################
@@ -112,6 +183,10 @@ def exec_menu(choice):
             if ch in ['9','0']:
                 menu_actions[ch]()
             elif ch == '1':
+                menu_actions[ch](layer)                
+                choice = input('>> ')
+                exec_menu(choice)
+            elif ch == '2':
                 menu_actions[ch](layer)                
                 choice = input('>> ')
                 exec_menu(choice)
@@ -133,8 +208,8 @@ def exit():
 #MENU DEFINITION
 menu_actions = {
         'main_menu': main_menu,
-        '1': VN2K2WGS84,
-        
+        '1': PRJVN2K2WGS84,
+        '2': PRJVN20002UTM,
         '9': back,
         '0': exit}
 ###############
